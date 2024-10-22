b505140e1258a64eba92841e0b7bd5100055cce0
myroslavkosyk81 <80488038+myroslavkosyk81@users.noreply.github.com>
2024-10-22 20:31:05 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
   SwiperCore.use([Navigation]);
-  // console.log(offerListings)
+  console.log(offerListings)
 
   const { t } = useTranslation();
   
@@ -22,9 +22,7 @@ export default function Home() {
     const fetchOfferListings = async () =>{
       try {
         const res = await fetch('/api/listing/get?offer=true&limit=4');
-        const data1 = await res.text();
-        console.log(data1)
-        const data = JSON.parse(data1);
+        const data = await res.json();
         setOfferListings(data);
         fetchRentListings();
       } catch (error) {
@@ -35,9 +33,7 @@ export default function Home() {
     const fetchRentListings = async () => {
       try {
         const res = await fetch('/api/listing/get?type=rent&limit=4');
-        const data1 = await res.text();
-        console.log(data1)
-        const data = JSON.parse(data1);
+        const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
       } catch (error) {
@@ -48,9 +44,7 @@ export default function Home() {
     const fetchSaleListings = async () => {
       try {
         const res = await fetch('/api/listing/get?type=sale&limit=4');
-        const data1 = await res.text();
-        console.log(data1)
-        const data = JSON.parse(data1);
+        const data = await res.json();
         setSaleListings(data);
       } catch (error) {
         console.log(error)
