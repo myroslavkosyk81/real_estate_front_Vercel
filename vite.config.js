966d9129fdea002c1eca17fa18f17d9c7b2fc08f
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2024-10-23 18:15:37 +0300

    vite.config plus unv

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+console.log(process.env.VITE_APP_API_URL)
+
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://real-estate-back-vercel.vercel.app',
+        // target: 'http://localhost:3000',
+        target: `${process.env.VITE_APP_API_URL}`,
         secure: false,
       },
     },
