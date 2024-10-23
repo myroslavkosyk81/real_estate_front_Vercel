6f66c05d955cb0bf41cbe8a007392eba316cb98b
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2024-10-23 18:21:21 +0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-<<<<<<< HEAD
         // target: 'http://localhost:3000',
         target: `${process.env.VITE_APP_API_URL}`,
-=======
-        //target: `${process.env.VITE_REACT_APP_BACKEND_BASEURL}`,
-        target: 'https://real-estate-back-vercel.vercel.app/',
-        
->>>>>>> de3be789db2352a269d3aa9c9cde2fa706f18ec8
         secure: false,
       },
     },
