e1e0d8bb269f5adcb862fe9093007a88a2186de8
myroslavkosyk81 <80488038+myroslavkosyk81@users.noreply.github.com>
2024-10-23 22:25:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig, loadEnv  } from 'vite'
 
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
 
-console.log(process.env.VITE_APP_API_URL)
+dotenv.config()
+//console.log(process.env.VITE_APP_API_URL)
 
 export default defineConfig({
   server: {
@@ -11,6 +13,17 @@ export default defineConfig({
         // target: 'http://localhost:3000',
         target: `${process.env.VITE_APP_API_URL}`,
         secure: false,
+        // configure: (proxy, _options) => {
+        //   proxy.on('error', (err, _req, _res) => {
+        //     console.log('proxy error', err);
+        //   });
+        //   proxy.on('proxyReq', (proxyReq, req, _res) => {
+        //     console.log('Sending Request to the Target:', req.method, req.url);
+        //   });
+        //   proxy.on('proxyRes', (proxyRes, req, _res) => {
+        //     console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+        //   });
+        // },
       },
     },
   },
