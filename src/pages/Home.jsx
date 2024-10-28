4102ded9093b8e8f8fea055de9d0fabfde811fd6
myroslavkosyk81 <80488038+myroslavkosyk81@users.noreply.github.com>
2024-10-28 22:30:09 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,11 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () =>{
       try {
-        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const res = await fetch('/api/listing/get?offer=true&limit=4', {
+          headers:{
+            accept: 'application/json',
+            'User-agent': 'learning app',
+          }});
         console.log(res)
         const data = await res.json();
         console.log(data)
@@ -35,7 +39,11 @@ export default function Home() {
 
     const fetchRentListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=rent&limit=4');
+        const res = await fetch('/api/listing/get?type=rent&limit=4', {
+          headers:{
+            accept: 'application/json',
+            'User-agent': 'learning app',
+          }});
         const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
@@ -46,7 +54,11 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const res = await fetch('/api/listing/get?type=sale&limit=4', {
+          headers:{
+            accept: 'application/json',
+            'User-agent': 'learning app',
+          }});
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
